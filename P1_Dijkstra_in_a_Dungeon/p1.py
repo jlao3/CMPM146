2ed856a9f8a142cdca28c61c5040f3666c85d574
jlao3 <65569051+jlao3@users.noreply.github.com>
2020-10-06 12:55:33 -0700

    Uhhhh, making sure stuff works

--- P1_Dijkstra_in_a_Dungeon/p1.py
+++ P1_Dijkstra_in_a_Dungeon/p1.py
@@ -104,7 +104,7 @@ def cost_to_all_cells(filename, src_waypoint, output_filename):
 
 
 if __name__ == '__main__':
-    filename, src_waypoint, dst_waypoint = 'example.txt', 'a','e'
+    filename, src_waypoint, dst_waypoint = 'example.txt', 'a', 'e'
 
     # Use this function call to find the route between two waypoints.
     test_route(filename, src_waypoint, dst_waypoint)
