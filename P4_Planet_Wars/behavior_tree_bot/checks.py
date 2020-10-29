ca7600b6de69a68482849276ddc76a6d4aa61dc8
jlao3 <65569051+jlao3@users.noreply.github.com>
2020-10-29 16:24:28 -0700

    Update

--- P4_Planet_Wars/behavior_tree_bot/checks.py
+++ P4_Planet_Wars/behavior_tree_bot/checks.py
@@ -8,14 +8,6 @@ def have_largest_fleet(state):
            > sum(planet.num_ships for planet in state.enemy_planets()) \
              + sum(fleet.num_ships for fleet in state.enemy_fleets())
 
-def not_attacking(state):
-  if not None in state.enemy_fleets():
-    for enemy_fleet in state.enemy_fleets():
-      if state.planets[enemy_fleet.destination_planet].owner == 1:
-        return False
-  else:
-    return True
-
 def is_attacking(state):
   if not None in state.enemy_fleets():
     for enemy_fleet in state.enemy_fleets():
